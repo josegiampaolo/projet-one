51fcb88e6afbf99d1d771641b67a63563cf34949
Josè Rafael Giampaolo <jose.giampaolo@aesys.tech>
2022-04-08 17:28:16 +0200

    Seconda Versione

---
+++ src/pescara/stampamionome.java
@@ -0,0 +1,10 @@
+package pescara;
+
+public class stampamionome {  // classe stampa mio nome
+
+    public static void main(String[] args) // metodo main
+    {
+        char stampanome[] = {'J', 'o', 's', 'è'}; //
+        System.out.println(stampanome); // stampa il mio nome
+    }
+}
